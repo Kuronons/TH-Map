0570480f3cc97841f987b2e93dbe9c1fa0fb50d0
spookyglitch84-png <spookyglitch84@gmail.com>
2026-03-02 14:46:27 +0000

    Add fruits.js with fruit data and seasons

---
+++ fruits.js
@@ -0,0 +1,15 @@
+const fruits = [
+  {
+    id: 1,
+    x: 62,
+    y: 41,
+
+    seasons: {
+      all: ["Azureberry", "Sunapple", "Ghost Berry"],
+      spring: ["Bloodberry", "Emberberry", "Nightshade Berry", "Skydrop"],
+      summer: ["Heartgleam", "Honeycran", "Nightblush", "solberry", "Tigermelon", "Velvetnight"],
+      fall: ["Amberburst", "Mellowspike", "Mireberry", "Mirthshade", "Scarletip", "Seafallow"],
+      winter: ["Faepeach", "Frostgleam", "Icerose", "Lunabright", "Twilipuff", "Velvitfrost"]
+    }
+  }
+];
